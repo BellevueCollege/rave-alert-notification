6f23d8a208cc855f1c710ce6e0b02e34f7b65c66
Nicole Swan <nicole.swan@bellevuecollege.edu>
2015-05-12 11:17:02 -0700

    Add Rave Alert archive configuration settings
    Changes add Rave Alert archive settings and related error checking.

--- alert-config.php
+++ alert-config.php
@@ -24,27 +24,37 @@ if (is_multisite() && current_user_can('manage_network'))  {
     <?php
 
     if (isset($_POST['action']) && $_POST['action'] == 'update_ravealert_settings') {
-
+        
         check_admin_referer('save_network_settings', 'my-network-plugin');
 
         //sample code from Professional WordPress book
 
         //store option values in a variable
         $network_settings = $_POST['network_settings'];
+
         $cacheCommand = $network_settings["ravealert_clearCacheCommand"];
         $cacheCommand = stripslashes($cacheCommand);
         $cacheCommand = base64_encode($cacheCommand);
         $network_settings["ravealert_clearCacheCommand"] = $cacheCommand;
-
+        
+        //if choose to archive, throw error if archive site not chosen
+        $submit_message = "";
+        if ( isset($network_settings["ravealert_do_archive"]) && $network_settings["ravealert_do_archive"] == "true" && empty($network_settings["ravealert_archive_site"]) ) {
+            //since archive site not chosen, change archive option back to false before saving options
+            $network_settings["ravealert_do_archive"] = "false";
+            $submit_message = '<div id="error" class="error fade"><p><strong>Select a site for the Rave Alert archive.</strong></p></div>';
+        } else {    
+            //otherwise assume it all went according to plan
+            $submit_message = '<div id="message" class="updated fade"><p><strong>Rave Alert network settings updated!</strong></p></div>';
+        }
+        
         //use array map function to sanitize option values
         $network_settings = array_map( 'sanitize_text_field', $network_settings );
-
+    
         //save option values
         update_site_option( 'ravealert_network_settings', $network_settings );
-
-        //just assume it all went according to plan
-        echo '<div id="message" class="updated fade"><p><strong>Rave Alert Network Settings Updated!</strong></p></div>';
-
+        
+        echo $submit_message;
     }//if POST
 
     ?>
@@ -53,6 +63,7 @@ if (is_multisite() && current_user_can('manage_network'))  {
         <input type="hidden" name="action" value="update_ravealert_settings" />
         <?php
         $network_settings = get_site_option( 'ravealert_network_settings' );
+        
         //$holiday = $network_settings['holiday']; //holiday setting example from Professional WordPress
         $high_alert = $network_settings['high_alert'];
 
@@ -70,6 +81,16 @@ if (is_multisite() && current_user_can('manage_network'))  {
         $ravealert_clearCacheCommand = base64_decode($network_settings['ravealert_clearCacheCommand']);
         $ravealert_college_openmessage = $network_settings['ravealert_college_openmessage'];
         $ravealert_xml_feedurl = $network_settings['ravealert_xml_feedurl'];
+        
+        $archive_alert = $network_settings['ravealert_do_archive'];
+        $true_archive_selected = "";
+        $false_archive_selected = "";
+        if($archive_alert == "true")
+        {
+            $true_archive_selected = "checked";
+        } else {
+            $false_archive_selected = "checked";
+        }
 
         wp_nonce_field('save_network_settings', 'my-network-plugin');
         ?>
@@ -114,6 +135,38 @@ if (is_multisite() && current_user_can('manage_network'))  {
                 </th>
                 <td>
                     <input type="text" size="80" name="network_settings[ravealert_clearCacheCommand]" value="<?php echo $ravealert_clearCacheCommand; ?>"/>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">
+                        Create Archive for Rave Alerts?
+                </th>
+                <td>
+                    <label for="ravealert_do_archive_yes"><input type="radio" id="ravealert_do_archive_yes" name="network_settings[ravealert_do_archive]" value="true" <?php echo $true_archive_selected; ?> /> Yes</label>
+                    <label for="ravealert_do_archive_no"><input type="radio" id="ravealert_do_archive_no" name="network_settings[ravealert_do_archive]" value="false" <?php echo $false_archive_selected; ?>/>  No</label>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">
+                    <label for="archive_site">
+                        Archive Site
+                    </label>
+                </th>
+                <td>
+                    <select name="network_settings[ravealert_archive_site]" id="archive_site">
+                        <option value=""></option>
+                    <?php $net_sites = wp_get_sites();
+                        foreach( $net_sites as $net_site ) {
+                            $site_selected = "";
+                            if ( (null != $network_settings["ravealert_archive_site"]) && $net_site["blog_id"] == $network_settings["ravealert_archive_site"]) { 
+                                   $site_selected = "selected";
+                            }
+                            $blog_detail = get_blog_details($net_site["blog_id"]);
+                            $blog_name = $blog_detail->blogname;
+                    ?>
+                        <option value="<?php echo $net_site["blog_id"]; ?>" <?php echo $site_selected; ?>><?php echo $blog_name; ?></option>
+                    <?php } ?>
+                    </select>
                 </td>
             </tr>
             <tr valign="top">
