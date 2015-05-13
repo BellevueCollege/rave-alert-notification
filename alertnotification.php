adda9f7bd224acefaa9f8c1396b6067886a718f5
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2015-05-13 15:46:08 -0700

    Add archive functionality to rave alerts.

--- alertnotification.php
+++ alertnotification.php
@@ -15,7 +15,10 @@ require_once("alert-config.php");
  * Putting the College open message functionality to run outside of cron job
  * */
 
+
+//add_action('wp_footer','getOpenMsg',100);
 $new_data = getOpenMsg();
+//error_log(print_r(get_current_site(),true));
 
 add_action( 'init', 'test_start_buffer', 0, 0 );
 
@@ -60,11 +63,14 @@ function is_login_page() {
 
 function getOpenMsg()
 {
+
     $network_settings = get_site_option( 'ravealert_network_settings' );
     $high_alert = $network_settings['high_alert'];
     $open_message = $network_settings['ravealert_college_openmessage'];
     $url = $network_settings['ravealert_xml_feedurl'];
     $new_data = cap_parse($url);
+    // var_dump($new_data);
+   
     $returnArray = array();
     if(!isset($new_data) || !isset($new_data["description"]) || empty($new_data["description"])) // if the description is empty or not set
     {
@@ -119,16 +125,26 @@ else
 {
     //error_log("my cron is already scheduled");
 }
-wp_cron();
+//wp_cron();
 
 
 function myCronFunction()
 {
     //error_log("############################CRON TAB is Running #######################");
+    
+
     $network_settings = get_site_option( 'ravealert_network_settings' );
     $url = $network_settings['ravealert_xml_feedurl'];//;get_template_directory() . '/inc/alert-notification/channel1.xml';
-    $new_data = cap_parse($url);
-    $getHtml = returnHtmlNClearCache($new_data);
+    $xml_data = cap_parse($url);
+    $getHtml = returnHtmlNClearCache($xml_data);
+    
+    $return_post_id = createRavePost($xml_data);
+    if($return_post_id)
+    {
+         //error_log("\n"."Error: Server ".$key." returns ".$value["return_value"]." while running command ".$value["command_run"]."\n");
+        //error_log("A new post is created with post id :".$return_post_id);
+    }
+    
 }
 /*
  * Parses the xml feed through CAP
@@ -140,7 +156,7 @@ function cap_parse($url){
     $xml = @simplexml_load_file($url);
     if($xml)
     {
-
+        $identifier = $xml->identifier;        
         $event = $xml->info->event;
         $description=$xml->info->description;
         $headline =$xml->info->headline;
@@ -153,6 +169,7 @@ function cap_parse($url){
         //Test to see if current time is between effective time and expire time
         if (($time > $sent) && ($time < $expires)) {
             //If true, print HTML using event and description info
+            $returnArray["identifier"] = $identifier;
             $returnArray["description"] = $description;
             $returnArray["class"] = "alert alert-danger alert-error";
             $returnArray["headline"] = $headline;
@@ -186,13 +203,20 @@ function returnHtmlNClearCache($new_data)
     }
     $new_display_message = !empty($new_data["event"]) ?  "<h3>".$new_data["event"]."</h3><p>".$new_data["headline"]."</p><p>".$new_data["description"]."</p>" : "";
 
+  
+
 //Clear the cache if there is a new message
     $check = compareCurrentNewMessage($new_display_message);
+   // error_log("check :".$check);
     if($check)
     {
         $cache_cleared = clearCache();
         if(!$cache_cleared)
             error_log("ERROR: CACHE IS NOT BEING CLEARED");
+
+       
+
+        
     }
     //Updated the new message and the class variable in the database
     updateCurrentMsg($new_display_message,$class);
@@ -244,8 +268,10 @@ function compareCurrentNewMessage($new_message)
     }
     if($currentMsg != $new_message)
     {
+        //error_log("Return true");
         return true;
     }
+     //error_log("Return false");
     return false;
 }
 function returnContentsOfUrl($url)
@@ -262,4 +288,63 @@ function updateCurrentMsg($new_display_message,$class)
     update_site_option("ravealert_currentMsg", $new_display_message);
     update_site_option("ravealert_classCurrentMsg", $class);
 }
+
+
+/*
+* Create new post 
+*/
+
+function createRavePost($xml_data)
+{
+    $post_return_value = 0;
+    //error_log("identifier :".$xml_data["identifier"]);
+     if(isset($xml_data) && isset($xml_data["event"]) && isset($xml_data["headline"]) && isset($xml_data["description"]) && isset($xml_data["identifier"]))
+     {
+        $event = $xml_data["event"] ;
+        $headline = $xml_data["headline"];
+        $description = $xml_data["description"];
+        $identifier = $xml_data["identifier"];
+
+        $all_site_information = wp_get_sites();
+        $network_settings = get_site_option( 'ravealert_network_settings' );
+        $check_to_archive = $network_settings['ravealert_do_archive'];
+        $archive_blog_id = $network_settings['ravealert_archive_site'];
+        $current_blog_id = get_current_blog_id();
+        error_log("blog id :".$archive_blog_id);
+        error_log("check archive:".$check_to_archive);
+        //get the blog id onto which the archive posts needs to be created.
+        if($check_to_archive == "true")
+        {           
+             switch_to_blog( $archive_blog_id );               
+        // Check if the post already exists. Creat a new post if it does not exist.
+          global $wpdb;
+
+          $post_id_exists = $wpdb->get_var($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_name=%s LIMIT 1", $identifier))  ;
+          //error_log("post id of the existing post :".$post_id_exists);             
+            if(!$post_id_exists)
+            { 
+                $description = $headline . "<!--more-->" . $description;
+                 $post_args = array(
+                    'post_name'   => $identifier,
+                    'post_title'  => $event,
+                    'post_content'  => $description,
+                    'post_status'   => 'publish',
+                    'post_author'   => 1,
+                );
+                $post_return_value = wp_insert_post( $post_args );                   
+                error_log("post return value:".$post_return_value);
+            }   
+            restore_current_blog();                       
+                                
+        }
+
+    }
+
+     return $post_return_value;
+}
+
+
+
+
+
 ?>
