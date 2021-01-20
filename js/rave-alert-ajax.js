441a0c53508671186780a7d77fd243f5c6bbfc58
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2021-01-20 09:18:10 -0800

    👌 IMPROVE: Stop Cache Busting REST Endpoints

--- js/rave-alert-ajax.js
+++ js/rave-alert-ajax.js
@@ -1,5 +1,5 @@
 /*
-* Pull data from xml feed
+* Pull emergency alert data from REST endpoint
 *
 */
 
@@ -7,11 +7,11 @@
 jQuery( document ).ready( function( $ ) {
     (function callAjax() {
         // Get minutes of current time for cache busting
-        var current_time = new Date();
-        var current_minutes = current_time.getMinutes(); // Should be a number, like 12, or 59
+        //var current_time = new Date();
+        //var current_minutes = current_time.getMinutes(); // Should be a number, like 12, or 59
         $.ajax({
             method: 'GET',
-            url: rest_php_variables['rest_url'] + '?' + current_minutes,
+            url: rest_php_variables['rest_url'],
         }).done(function (alert_info) {
 
             // If there is an Alert via CAP XML
@@ -30,7 +30,7 @@ jQuery( document ).ready( function( $ ) {
                     
                     $.ajax({
                         method: 'GET',
-                            url: rest_php_variables['rest_url'] + alert_info['identifier'] + '?' + current_minutes,
+                            url: rest_php_variables['rest_url'] + alert_info['identifier'],
                     }).done(function (data) {
 
                         var output = '';
