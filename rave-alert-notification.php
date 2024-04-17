615323002ea888b6b79b68e24a2badda249b20a3
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-04-17 10:02:58 -0700

    Update Version to 1.10

--- rave-alert-notification.php
+++ rave-alert-notification.php
@@ -4,7 +4,7 @@ Plugin Name: Rave Alert Notification
 Plugin URI: https://github.com/BellevueCollege/rave-alert-notification
 Description: Sends Rave Alert notification to Bellevue College WordPress sites.
 Author: Bellevue College IT Services
-Version: 1.9.2
+Version: 1.10
 Author URI: https://www.bellevuecollege.edu
 GitHub Plugin URI: bellevuecollege/rave-alert-notification
 Text Domain: rave-alert-notification
@@ -58,7 +58,7 @@ function bc_rave_enqueue_ajax() {
                                                     open_message_class: "' . $open_message_class . '",
                                                     is_homepage: "' . $is_homepage . '"
                                                 };';
-    wp_enqueue_script( 'rave-alert-ajax', plugin_dir_url( __FILE__ ) . 'js/rave-alert-ajax.js#asyncdeferload', array('jquery'), '1.9.2', true );
+    wp_enqueue_script( 'rave-alert-ajax', plugin_dir_url( __FILE__ ) . 'js/rave-alert-ajax.js#asyncdeferload', array('jquery'), '1.10.0', true );
     wp_add_inline_script( 'rave-alert-ajax', $rest_variables, 'before' );
 
 }
